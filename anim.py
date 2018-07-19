8c003df4084d28ee91cb228837057824fa7f3c1b
starryyy <sianochara@gmail.com>
2018-07-19 12:39:26 +0100

    improve loading

--- anim.py
+++ anim.py
@@ -5,14 +5,15 @@ from time import sleep
 anim_base="anim/"
 rgx_meta = re.compile(r'^[\s\S]+---+')
 rgx_comment=re.compile(r'\/\*(\*(?!\/)|[^*])*\*\/|\/\/.+')
+rgx_command=re.compile(r'[a-zA-Z]+')
 
 def naturalSort(l):
     l.sort(key=lambda x:[(int(c) if c.isdigit() else c) for c in re.split('([0-9]+)', x)])
     return l
 
-def load(name):
-    if os.path.exists(anim_base+name):
-        return [x for x in naturalSort(os.listdir(anim_base+name)) if x!="data"]
+# def load(name):
+#     if os.path.exists(anim_base+name):
+#         return [x for x in naturalSort(os.listdir(anim_base+name)) if x!="data"]
 
 def read(file):
     with(open(file,"r")) as f:
@@ -21,8 +22,8 @@ def read(file):
 def clear():
     _=os.system('cls' if os.name == 'nt' else 'clear')
 
-def format(str):
-        return [x.strip() for x in rgx_comment.sub('',str).splitlines()]
+def format(s):
+        return filter(None,[x.strip() for x in rgx_comment.sub('',s).splitlines()])
 
 class player:
     def __init__(self, name,autoLoad=True):
@@ -31,25 +32,33 @@ class player:
 
         #defaults
         self.interval=600
+        self.defined=False
+        self.metadata=[]
+        self.data=[]
 
         if autoLoad:self.load()
 
     def load(self):
-        self.scenes=load(self.name)
+        # self.scenes=load(self.name)
+        if not os.path.exists(self.root):
+            return
 
         self.defined = os.path.exists(self.root+"/data")
         if self.defined:
             self.rawdata = read(self.root+"/data")
             for match in rgx_meta.finditer(''.join(self.rawdata)):
                 self.metadata=format(match.group(0))
-                self.data = format((''.join(self.rawdata))[match.endpos-1:])
+                self.data = format((''.join(self.rawdata))[len(match.group(0)):])
                 break
+            else:
+                self.data=format(''.join(self.rawdata))
+                print 'no meta'
             self.loadMeta()
 
-    def loadMeta(self):
+    def loadMeta(self): #todo: centre, density, etc
         n=0
         for line in self.metadata:
-            if line.sub('-','').strip()=='':
+            if line.replace('-','').strip()=='':
                 break
             else:
                 if n==0:
@@ -58,12 +67,16 @@ class player:
 
     def drawScene(self,scene):
         print ''.join(read(self.root+"/"+scene))
+    
+    def doLine(self,line):
+        pass
 
     def play(self):
         if self.defined:
-            pass
+            for line in self.data:
+                self.doLine(line)
         else:
-            for scene in self.scenes:
+            for scene in [x for x in naturalSort(os.listdir(anim_base+self.name)) if x!="data"]:
                 clear()
                 self.drawScene(scene)
                 sleep(self.interval/1000.0)
--- syntax.md
+++ syntax.md
@@ -1,6 +1,6 @@
 1000 //first line interval
 --- //metadata end
-1 //scene no
-"3" //scene name
+1 //scene name
+3 //scene name
 w 5000 //wait 5s
 w +5 //relative to interval
